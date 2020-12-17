7abb1df11dd8319b067af742785fcd97c98f734d
AntonPuY25 <puy25@bk.ru>
2020-12-17 22:25:27 +0300

    reactRouterDom

--- src/Profile/Posts/Post/Post.tsx
+++ src/Profile/Posts/Post/Post.tsx
@@ -8,7 +8,6 @@ export default function Post(props:PostType){
     return(<div className={s.post}>
 
 
-        <img src={'https://steamuserimages-a.akamaihd.net/ugc/931551805216142162/8CA459BD2EF031FF144884A2E9DD3B07689CC883/?imw=512&amp;imh=531&amp;ima=fit&amp;impolicy=Letterbox&amp;imcolor=%23000000&amp;letterbox=true'}/>
         <div>
             <span>Like:</span>
             <span>{props.like}</span>
