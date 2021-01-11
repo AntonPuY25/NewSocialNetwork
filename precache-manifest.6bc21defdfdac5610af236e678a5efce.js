c135119778be954d0bbbe11115f281a83a3127f6
AntonPuY25 <puy25@bk.ru>
2021-01-11 18:43:32 +0300

    Updates

---
+++ precache-manifest.6bc21defdfdac5610af236e678a5efce.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e47aa41769fc0127b39a5548dc9385f",
+    "url": "/NewSocialNetwork/index.html"
+  },
+  {
+    "revision": "a777a4829ad3109d91af",
+    "url": "/NewSocialNetwork/static/css/main.10cae67b.chunk.css"
+  },
+  {
+    "revision": "9dfc7687a629f8e28b46",
+    "url": "/NewSocialNetwork/static/js/2.32f305ca.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/NewSocialNetwork/static/js/2.32f305ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a777a4829ad3109d91af",
+    "url": "/NewSocialNetwork/static/js/main.a49e3e68.chunk.js"
+  },
+  {
+    "revision": "5fb735c09589394ca536",
+    "url": "/NewSocialNetwork/static/js/runtime-main.a86eb3e9.js"
+  },
+  {
+    "revision": "efeaeea871c17859a3d0d995adaafbef",
+    "url": "/NewSocialNetwork/static/media/ava1.efeaeea8.png"
+  },
+  {
+    "revision": "2189f5297ca79973b58985ce73810b96",
+    "url": "/NewSocialNetwork/static/media/ava2.2189f529.png"
+  },
+  {
+    "revision": "778a408fb7f4b54398a97b3242ed6a7a",
+    "url": "/NewSocialNetwork/static/media/fon.778a408f.jpg"
+  },
+  {
+    "revision": "9af25c38ec76032b8b450285fd29130c",
+    "url": "/NewSocialNetwork/static/media/friends.9af25c38.png"
+  },
+  {
+    "revision": "e8b340f0657f43f31e4cf1cf3f6b83fc",
+    "url": "/NewSocialNetwork/static/media/post1.e8b340f0.jpg"
+  },
+  {
+    "revision": "aef379ad89acc583951d66f07efe3092",
+    "url": "/NewSocialNetwork/static/media/post2.aef379ad.jpg"
+  },
+  {
+    "revision": "92c25f37d91bbc70ba1a197b38c0b57c",
+    "url": "/NewSocialNetwork/static/media/settings.92c25f37.png"
+  }
+]);
