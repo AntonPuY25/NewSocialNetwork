ac8d782f30fdd27dd8dde5408f1464459ccd912d
AntonPuY25 <puy25@bk.ru>
2021-01-24 11:43:53 +0300

    Test_Auth

--- src/Header/header.tsx
+++ src/Header/header.tsx
@@ -2,16 +2,15 @@ import React from "react";
 import s from './header.module.css';
 import {AppBar, Button, IconButton, Toolbar} from "@material-ui/core";
 import {TypePropsHeader} from "../Types/Types";
-
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 const Header:React.FC<TypePropsHeader>=({isAuth,email})=>{
     return(<div className={s.header} >
         <AppBar position="static" color={"transparent"}>
             <Toolbar>
                 <IconButton edge="start" color="inherit" aria-label="menu">
-                    <div>
-                        <img alt={'logo'} src={"https://cdn.pixabay.com/photo/2018/05/27/22/48/social-3434839_1280.png"}/>
 
-                    </div>
+                    <LinkedInIcon style={{ color: 'white' }}  fontSize={"large"}/>
+
                 </IconButton>
                 {isAuth ? <div className={s.email}>{email}</div>: <Button variant={"text"} >
                     <div className={s.login}>Login</div></Button>}
